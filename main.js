11b94195924e5e310c25d724a2660ca5de510b46
Marta Kornacka <40639586+martusskornacka@users.noreply.github.com>
2019-07-22 22:36:26 +0200

    New menu and collapsible

--- main.js
+++ main.js
@@ -45,3 +45,18 @@ const addTask = (e) => {
 //     })
 // }
 form.addEventListener('submit', addTask)
+
+const coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener('click', function () {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    })
+}
