ad501dc590fefc7178605a65645cace049020987
Marta Kornacka <40639586+martusskornacka@users.noreply.github.com>
2019-07-25 22:14:32 +0200

    Add clock

--- main.js
+++ main.js
@@ -1,49 +1,42 @@
+
+const toDoList = [];
+
 const form = document.querySelector('form');
+const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h4 span');
 const listItems = document.getElementsByClassName('task');
-let ul = document.getElementById('lists');
-const btn = document.querySelector('input.second');
-const todoList = [];
+const input = document.querySelector('input.second');
 
 const removeTask = (e) => {
-    ul.textContent = "";
-    e.target.parentnode.remove();
+    e.target.parentNode.remove();
     const index = e.target.parentNode.dataset.key;
-    toDoList.splice(index, 1);
+    toDoList.splice(index, 1)
     taskNumber.textContent = listItems.length;
+    renderList();
+}
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = input.value;
+    if (titleTask === "") return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = titleTask + " <button>Usuń</button>";
+    toDoList.push(task);
+    renderList()
+
+    ul.appendChild(task);
+    input.value - "";
+    taskNumber.textContent = listItems.length;
+    task.querySelector('button').addEventListener('click', removeTask);
+}
+const renderList = () => {
     ul.textContent = "";
     toDoList.forEach((toDoElement, key) => {
         toDoElement.dataset.key = key;
         ul.appendChild(toDoElement);
     })
-
-    // renderList();
-}
-const addTask = (e) => {
-    e.preventDefault();
-    const titleTask = btn.value;
-    if (titleTask === '') return;
-    const task = document.createElement('li');
-    task.className = 'task';
-    task.innerHTML = titleTask + '<button>Usuń</button>';
-    todoList.push(task);
-
-    // renderList()
-
-    ul.appendChild(task);
-    btn.value = "";
-    taskNumber.textContent = listItems.length;
-    task.querySelector('button').addEventListener('click', removeTask)
 }
 
-// const renderList = () => {
-//     ul.textContent = "";
-//     toDoList.forEach((toDoElement, key) => {
-//         toDoElement.dataset.key = key;
-//         ul.appendChild(toDoElement);
-//         console.log(toDoElement)
-//     })
-// }
 form.addEventListener('submit', addTask)
 
 const coll = document.getElementsByClassName("collapsible");
@@ -60,3 +53,32 @@ for (i = 0; i < coll.length; i++) {
         }
     })
 }
+
+
+const endTime = new Date('2019-12-31 00:01:00').getTime();
+
+const spanD = document.querySelector('span.d');
+const spanH = document.querySelector('span.h');
+const spanM = document.querySelector('span.m');
+
+
+setInterval(() => {
+    const nowTime = new Date().getTime();
+    // const time = Math.floor((endTime - nowTime) / 1000);
+    const time = endTime - nowTime;
+    const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+
+
+    let hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+    // Przykład - dodanie 0 przeg godziną 
+    hours = hours < 10 ? `0${hours}` : hours;
+
+    const minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+
+
+    spanD.textContent = days;
+    spanH.textContent = hours;
+    spanM.textContent = minutes;
+
+}, 1000)
+
