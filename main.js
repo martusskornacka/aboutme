d387f86189a516188864aa28af414d672c95f183
Marta Kornacka <40639586+martusskornacka@users.noreply.github.com>
2019-07-28 21:23:58 +0200

    Add toDoList

--- main.js
+++ main.js
@@ -1,42 +1,49 @@
-
-const toDoList = [];
-
 const form = document.querySelector('form');
-const ul = document.querySelector('ul');
 const taskNumber = document.querySelector('h4 span');
 const listItems = document.getElementsByClassName('task');
-const input = document.querySelector('input.second');
+let ul = document.getElementById('lists');
+const btn = document.querySelector('input.second');
+const todoList = [];
 
 const removeTask = (e) => {
-    e.target.parentNode.remove();
+    ul.textContent = "";
+    e.target.parentnode.remove();
     const index = e.target.parentNode.dataset.key;
-    toDoList.splice(index, 1)
+    toDoList.splice(index, 1);
     taskNumber.textContent = listItems.length;
-    renderList();
-}
-const addTask = (e) => {
-    e.preventDefault();
-    const titleTask = input.value;
-    if (titleTask === "") return;
-    const task = document.createElement('li');
-    task.className = 'task';
-    task.innerHTML = titleTask + " <button>Usuń</button>";
-    toDoList.push(task);
-    renderList()
-
-    ul.appendChild(task);
-    input.value - "";
-    taskNumber.textContent = listItems.length;
-    task.querySelector('button').addEventListener('click', removeTask);
-}
-const renderList = () => {
     ul.textContent = "";
     toDoList.forEach((toDoElement, key) => {
         toDoElement.dataset.key = key;
         ul.appendChild(toDoElement);
     })
+
+    // renderList();
+}
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = btn.value;
+    if (titleTask === '') return;
+    const task = document.createElement('li');
+    task.className = 'task';
+    task.innerHTML = titleTask + '<button>Usuń</button>';
+    todoList.push(task);
+
+    // renderList()
+
+    ul.appendChild(task);
+    btn.value = "";
+    taskNumber.textContent = listItems.length;
+    task.querySelector('button').addEventListener('click', removeTask)
 }
 
+// const renderList = () => {
+//     ul.textContent = "";
+//     toDoList.forEach((toDoElement, key) => {
+//         toDoElement.dataset.key = key;
+//         ul.appendChild(toDoElement);
+//         console.log(toDoElement)
+//     })
+// }
 form.addEventListener('submit', addTask)
 
 const coll = document.getElementsByClassName("collapsible");
@@ -82,3 +89,90 @@ setInterval(() => {
 
 }, 1000)
 
+//slider
+
+
+const slideList = [{
+    img: "images/blueberry.jpg",
+    text: 'Pierwszy tekst'
+},
+{
+    img: "images/pancake.jpg",
+    text: 'Drugi tekst'
+},
+{
+    img: "images/main.jpg",
+    text: 'Trzeci tekst'
+}];
+
+const image = document.querySelector('img.slider');
+const h1 = document.querySelector('h1.slider');
+const dots = [...document.querySelectorAll('.dots span')]
+// Interfejs
+const time = 3000;
+let active = 0; // które elementy są wyswietlane, w lewo cofamy to -1
+
+// Implementacje
+
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[activeDot].classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+const changeSlide = () => {
+    active++;
+    if (active === slideList.length) {
+        active = 0;
+    }
+    image.src = slideList[active].img;
+    changeDot()
+}
+let indexInteval = setInterval(changeSlide, time)
+
+const keyChangeSlide = (e) => {
+    if (e.keyCode == 37 || e.keyCode == 39) {
+        clearInterval(indexInteval); //wyłaczamy interwał
+        e.keyCode == 37 ? active-- : active++; //powiększamy lub zmniejszamy active 
+        if (active === slideList.length) {
+            active = 0; //to zerujemy wartość
+        } else if (active < 0) {
+            active = slideList.length - 1; //cofamy o 1 bo tyle mamy index
+        }
+
+        image.src = slideList[active].img;
+        h1.textContent = slideList[active].text;
+        changeDot()
+        indexInteval = setInterval(changeSlide, time)
+        //setInterval(changeSlide, time) //uruchomiony, ale przy następnym kliknięciu nie będzie indexu, nie wykona się, kade kliknięcie wywołuje setInterval
+    }
+}
+// utwórz funkcje keyChangeSlide. Zadanie może wymagać także zmian poza funkcją.
+window.addEventListener('keydown', keyChangeSlide)
+
+$(document).ready(
+    function () {
+        $('#newList').click(
+            function () {
+                let toAdd = $('input[name=input2]').val();
+                $('ol').append('<p>' + toAdd + '</p>');
+            });
+
+        $("input[name=input2]").keyup(function (event) {
+            event.preventDefault();
+            if (event.keyCode == 13) {
+                $("#newList").click();
+            }
+        });
+
+        $(document).on('dblclick', 'p', function () {
+            $(this).toggleClass('strike').fadeOut('slow');
+        });
+
+        $('.texty').focus(function () {
+            $(this).val('');
+        });
+        $('ol').sortable();
+
+    }
+);
